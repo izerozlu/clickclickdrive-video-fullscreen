9f6ff2eedb3641558d35a45f9f1ff3a60daf9463
izerozlu <izerozlu@gmail.com>
2023-10-19 10:15:02 +0200

    Update README to include behaviour explanation, use stylesheets instead of inline JS styles and improve behaviour

--- README.md
+++ README.md
@@ -25,3 +25,13 @@ A file dialog will appear. Navigate to the folder where you have downloaded the 
 ## Step 6: Install the Extension
 
 Chrome will now load the extension from the selected folder. You should see the extension's icon appear in the extensions bar.
+
+# How to use it?
+
+**Click to Open the Video:** To open the video in a bigger view, click on the video player. A larger window will appear, showing the video more prominently. This way, you can focus on the video and enjoy it fully. But keep in mind that clickclickdrive.de's videos need an initial click to enable the video. After that you can open the dialog with each click to the video.
+
+**Answer Questions:** Below the video, you'll see a list of questions. Click on the questions to answer them. The answers in the dialog and the original ones are connected via `click` events. So you can choose the answers inside the dialog to actually select answers.
+
+**Close the Window:** If you want to go back to the regular view or close the video dialog, simply click anywhere outside the video window. You can also press the "Escape" key on your keyboard, and the video dialog will close.
+
+**Automatic Opening:** When a new video starts playing (for example when you continue to the next video question), the video dialog will open automatically. This is convenient, so you don't need to click the video player each time.
--- content-scripts/initial-page.js
+++ content-scripts/initial-page.js
@@ -1,74 +1,73 @@
+const openDialog = (videoElement, previousVideoSrc) => {
+    previousVideoSrc(videoElement.src);
+    videoElement.pause();
+
+    const container = document.createElement('div');
+    container.classList.add('container');
+
+    const title = document.createElement('h1');
+    title.textContent = document.querySelector('h1').textContent;
+    title.classList.add('title');
+
+    const video = document.createElement('video');
+    video.classList.add('video')
+    video.src = previousVideoSrc();
+    video.controls = true;
+    video.autoplay = true;
+
+    const answersContainer = document.createElement('div');
+    answersContainer.classList.add('answers-container');
+
+    Array.from(document.querySelectorAll('form li')).forEach((answer) => {
+        const button = document.createElement('button');
+        button.classList.add('answer');
+        button.textContent = answer.textContent;
+        button.addEventListener('click', () => {
+            answer.querySelector('label').dispatchEvent(new MouseEvent('click'));
+            button.classList.toggle('selected');
+        });
+
+        answersContainer.appendChild(button);
+    });
+
+    container.appendChild(title);
+    container.appendChild(video);
+    container.appendChild(answersContainer);
+
+    container.addEventListener('click', (event) => {
+        if (![video, ...document.querySelectorAll('.answers-container .answer')].includes(event.target)) {
+            container.remove();
+        }
+    });
+
+    document.querySelector('body').appendChild(container);
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'Escape') {
+            container.remove();
+        }
+    });
+};
+
 chrome.runtime.sendMessage(
     {action: 'doSomething'},
     async function () {
-        let hasVideoFound = false;
-        let previousVideoSrc;
+        let _previousVideoSrc;
+
+        const previousVideoSrc = (newSrc) => {
+            if (newSrc) {
+                _previousVideoSrc = newSrc;
+            }
+
+            return _previousVideoSrc;
+        };
 
         setInterval(() => {
             const videoElement = document.querySelector('video');
-
-            if (videoElement?.src !== previousVideoSrc) {
-                previousVideoSrc = videoElement.src;
-
-                const container = document.createElement('div');
-                container.style.position = 'fixed';
-                container.style.top = '0';
-                container.style.left = '0';
-                container.style.width = '100vw';
-                container.style.height = '100vh';
-                container.style.background = 'rgba(0, 0, 0, 0.8)';
-                container.style.display = 'flex';
-                container.style.flexDirection = 'column';
-                container.style.justifyContent = 'center';
-                container.style.alignItems = 'center';
-                container.style.padding = '20px';
-
-                const video = document.createElement('video');
-                video.src = previousVideoSrc;
-                video.controls = true;
-                video.autoplay = true;
-                video.style.width = '100%';
-                video.style.borderRadius = '10px';
-                video.style.maxWidth = '80vw';
-
-                const answersContainer = document.createElement('div');
-                answersContainer.style.display = 'flex';
-                answersContainer.style.justifyContent = 'space-between';
-                answersContainer.style.alignItems = 'center';
-                answersContainer.style.width = '100%';
-                answersContainer.style.paddingTop = '32px';
-                answersContainer.style.gap = '32px';
-
-                Array.from(document.querySelectorAll('form li')).forEach((answer) => {
-                    const span = document.createElement('span');
-                    span.textContent = answer.textContent;
-                    span.style.background = 'rgba(255, 255, 255, 0.8)';
-                    span.style.padding = '16px';
-                    span.style.borderRadius = '5px';
-                    span.style.display = 'flex';
-                    span.style.justifyContent = 'center';
-                    span.style.alignItems = 'center';
-                    span.style.height = '120px';
-                    span.style.textAlign = 'center';
-
-                    answersContainer.appendChild(span);
-                });
-                container.appendChild(video);
-                container.appendChild(answersContainer);
-
-                container.addEventListener('click', (event) => {
-                    if (event.target === container) {
-                        container.remove();
-                    }
-                });
-
-                document.querySelector('body').appendChild(container);
-
-                document.addEventListener('keydown', (event) => {
-                    if (event.key === 'Escape') {
-                        container.remove();
-                    }
-                });
+            if (videoElement && videoElement.src !== previousVideoSrc()) {
+                videoElement?.addEventListener('click', () => openDialog(videoElement, previousVideoSrc))
+            }
+            if (videoElement?.src !== previousVideoSrc()) {
+                openDialog(videoElement, previousVideoSrc);
             }
         }, 1000);
     }
